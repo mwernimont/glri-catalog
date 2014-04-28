6ea3e8006b0c306e09ef016527371cc662ffbbcc
Eric Everman <eeverman@usgs.gov>
2014-04-28 15:05:30 -0500

    Better implementation for non-production (Google Analytics disabled).

--- glri-catalog-ui/src/main/webapp/js/app/cida-analytics.js
+++ glri-catalog-ui/src/main/webapp/js/app/cida-analytics.js
@@ -1,11 +1,20 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-var enableSiteAnalytics = '${cida.google.analytics.enabled}'=='true';
-if (enableSiteAnalytics) {
+if ('${cida.google.analytics.enabled}'=='true') {
+	
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	
 	ga('set', 'anonymizeIp', true);
 	ga('create', '${cida.google.analytics.trackingid}', '${cida.google.analytics.domain}');
 	ga('send', 'pageview');
+	
+} else {
+	
+	console.log("Google Analytics is disabled for this non-production build.  Logging all ga calls to the console instead.");
+	
+	//Create dummy ga function that code can still use even though the Google api was not loaded.
+	ga = function(arg1, arg2, arg3) {
+		console.log("ga ignoring: " + arg1 + ", " + arg2 + ", '" + arg3 + "'");
+	}; 
 }
